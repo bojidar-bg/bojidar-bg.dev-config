92664848083efc12a49f823b9a1183b119118a6e
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2024-03-16 19:32:59 +0200

    rclone plugin for videos

--- gen-passwords.sh
+++ gen-passwords.sh
@@ -2,7 +2,7 @@
 
 [ -f .env ] && source .env
 
-[ -z "$ZULIP_POSTGRESS_PASS" ] && echo "ZULIP_POSTGRESS_PASS="$(openssl rand -base64 15) >> .env
+[ -z "$ZULIP_POSTGRESS_PASS" ] && echo "ZULIP_POSTGRESS_PASS="$(openssl rand -base64 15) >> .env ### docker exec zulip-postgres bash -c $'psql -U"$POSTGRES_USER" -c "ALTER ROLE $POSTGRES_USER  WITH PASSWORD \'$POSTGRES_PASSWORD\';"'
 [ -z "$ZULIP_MEMCACHED_PASS" ] && echo "ZULIP_MEMCACHED_PASS="$(openssl rand -base64 15) >> .env
 [ -z "$ZULIP_RABBITMQ_PASS" ] && echo "ZULIP_RABBITMQ_PASS="$(openssl rand -base64 15) >> .env
 [ -z "$ZULIP_REDIS_PASS" ] && echo "ZULIP_REDIS_PASS="$(openssl rand -base64 15) >> .env
